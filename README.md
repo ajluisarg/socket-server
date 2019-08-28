818f49ea045ae8349f2c61972bb7694486b33799
Ángel José <ajluisarg@gmail.com>
2019-08-28 20:04:39 +0200

    socket implemented

---
+++ README.md
@@ -0,0 +1,5 @@
+# Socket-Server
+
+````
+npm install
+```
--- class/server.ts
+++ class/server.ts
@@ -1,17 +1,39 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
-
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server{
      public app: express.Application;
      public port: number;
+     public io: socketIO.Server;
 
-     constructor(){
+     private httpServer: http.Server;
+
+     private static _instance: Server;
+
+     private constructor(){
          this.app = express();
          this.port = SERVER_PORT;
+         this.httpServer = new http.Server(this.app);
+         this.io = socketIO(this.httpServer);
+         this.listenSockets();
+     }
+
+     public static get instance(){
+        return this._instance || (this._instance = new this())
      }
 
      public start(callback: ()=>void){
-         this.app.listen(this.port, callback);
+         this.httpServer.listen(this.port, callback);
+     }
+
+
+     private listenSockets(){
+         console.log('listening socket connections');
+         this.io.on('connection', client =>{
+             console.log('client connected');
+             
+         })
      }
 }
